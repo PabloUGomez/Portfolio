0e1e2919914074a0284897a36b817fdcc4d34c07
Pablo Gomez <pablogomez0430@gmail.com>
2024-06-11 13:47:58 -0300

    add trello clone

--- src/store/projects.js
+++ src/store/projects.js
@@ -1,10 +1,35 @@
 export const projects = [
   {
     id: 1,
+    title: 'Trello Clone',
+    description:
+      'FullStack Trello clone: real-time board management.',
+    imageUrl: '/dashboard.webp',
+    imageUrl2: '/marketingPage.webp',
+    state: 'live',
+    year: '2024',
+    tech: [
+      'NextJs',
+      'Tailwind',
+      'TypeScript',
+      'Clerk',
+      'Prisma',
+      'SQLite',
+      'Stripe',
+    ],
+    link: 'https://trello-clone-liart-eight.vercel.app/',
+    publishDate: 'Published on June 11, 2024',
+    currentState: 'This project is currently finished and live.',
+    fullDescription:
+      'Explore the Trello clone, boasting full graphical interface alongside core functionalities: real-time board management, drag-and-drop functionality, and user authentication. Create, edit, and delete boards, list and cards with ease. ',
+
+  },
+  {
+    id: 2,
     title: 'Spotify Clone',
     description: 'Experience the Spotify clone: real-time music playback.',
     fullDescription:
-      ' Explore the Spotify clone, boasting full graphical interface alongside core functionalities: seamless music playback, adjustable volumecontrols, and the option to mute. Enjoy an immersive music experiencewith a familiar interface.',
+      ' Explore the Spotify clone, boasting full graphical interface alongside core functionalities: seamless music playback, adjustable volumecontrols, and the option to mute. Enjoy an immersive music experience with a familiar interface.',
     imageUrl: '/spotifyClone2.webp',
     imageUrl2: '/spotifyClone.webp',
     state: 'live',
@@ -21,24 +46,23 @@ export const projects = [
     publishDate: 'Published on April 9, 2024',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Weather App',
-    description:
-      'Check the weather in real time, with a focus on performance.',
+    description: 'Check the weather in real time, with a focus on performance.',
     imageUrl: '/weather.webp',
     imageUrl2: '/weather2.webp',
     state: 'building',
     year: '2024',
-    tech: ['NextJs','React', 'TypeScript', 'Tailwind'],
+    tech: ['NextJs', 'TypeScript', 'Tailwind'],
     link: 'Building...',
     publishDate: 'Published on April 19, 2024',
     currentState:
       'This project is currently in progress, following the idea of ​​creating a weather app with a focus on performance.',
     fullDescription:
-    'Check the weather in real time with our web platform! Our weather app is designed with a focus on performance, providing you with up-to-date weather information in a clean and easy-to-use interface. Stay informed and plan your day with our weather app.',
+      'Check the weather in real time with our web platform! Our weather app is designed with a focus on performance, providing you with up-to-date weather information in a clean and easy-to-use interface. Stay informed and plan your day with our weather app.',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Typing Game',
     description:
       'Test your speed and accuracy against the clock or word limits.',
@@ -52,10 +76,10 @@ export const projects = [
     currentState:
       'This project is currently finished, following the idea of ​​making a type game similar to MonkeyType.',
     fullDescription:
-    'Experience the ultimate typing challenge on our web platform! Test your speed and accuracy against the clock or word limits. Track your words per minute and precision as you aim for mastery.',
+      'Experience the ultimate typing challenge on our web platform! Test your speed and accuracy against the clock or word limits. Track your words per minute and precision as you aim for mastery.',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Tesla Clone',
     description:
       'Experience the Tesla clone: with screen slide, animations, and video background.',
@@ -63,7 +87,7 @@ export const projects = [
     imageUrl2: '/teslaClone2.webp',
     state: 'live',
     year: '2023',
-    tech: ['Astro','JavaScript', 'Tailwind'],
+    tech: ['Astro', 'JavaScript', 'Tailwind'],
     link: 'https://tesla-clon-silk.vercel.app/',
     publishDate: 'Published on April 12, 2024',
     currentState:
@@ -72,7 +96,7 @@ export const projects = [
       'Explore the Tesla clone, boasting full graphical interface alongside core functionalities: seamless screen slide, animations, and video background. Enjoy an immersive experience with a familiar interface.',
   },
   {
-    id: 5,
+    id: 6,
     title: 'Npm Router Package',
     description:
       'A simple router package for npm, with a focus on performance.',
